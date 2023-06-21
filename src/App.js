7fa671cc6429890e36159cf4aecebe938cab951d
dumsterB <dumsterfordevelop@gmail.com>
2023-06-21 14:28:01 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useEffect } from 'react';
+import {Messenger } from 'messanger-ai';
+import 'messanger-ai/style.css';
+const App = () => {
+  useEffect(() => {
+    const config = {
+      holder: 'messanger',
+      picture:'',
+      token:'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',
+      socials: [
+        { link: "https://facebook.com/name_of_account", type: "facebook" },
+        { link: "https://twitter.com/name_of_account", type: "twitter" }
+      ],
+      tools: [], // Specify the desired tools here
+      name: 'React App Ai',
+    };
+    const messenger = new Messenger(config); // Use the correct path to the default object
+    
+    return () => {
+      // Clean up on component unmount, if necessary
+      // For example: messenger.destroy();
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="messanger"></div>
     </div>
   );
-}
+};
 
 export default App;
